333336da0ad2f5f6903e49dc11c969a677d2f9df
Ilfat <wd.richh@gmail.com>
2024-04-26 13:30:46 +0300

    221Spring

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -4,8 +4,11 @@ import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+
 import java.util.List;
 
 @Repository
@@ -22,8 +25,23 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
 
+   @Override
+   @Transactional
+   public User getUserByCarModelAndSeries(String model, int series) {
+//        String query = "from User where car.model=: modelParam and car.series=: seriesParam";
+      String query = "from User";
+
+      List<User> users = sessionFactory.getCurrentSession().createQuery(query).getResultList();
+      for (User user : users) {
+         if (user.getCar().getModel().equals(model) && user.getCar().getSeries() == series) {
+            return user;
+         }
+      }
+      return null;
+//            return sessionFactory.getCurrentSession().createQuery(query, User.class).setParameter("modelParam", model).setParameter("seriesParam", series).getSingleResult();
+   }
 }
--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,9 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {
+      return userDao.getUserByCarModelAndSeries(model, series);
+   }
+
 }
